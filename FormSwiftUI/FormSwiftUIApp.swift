a04de5073ce9fcc7f710d8a618ec63181362c149
Erikson Loures <erikson992@gmail.com>
2023-06-03 15:26:08 -0300

    Initial Commit

---
+++ FormSwiftUI/FormSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormSwiftUIApp.swift
+//  FormSwiftUI
+//
+//  Created by Erik Loures on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FormSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
