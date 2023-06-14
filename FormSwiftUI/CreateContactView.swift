411d59c3fb4994593ce4eb5030d0e2dd6d626ad3
Erikson Loures <erikson992@gmail.com>
2023-06-13 23:35:02 -0300

    first commit

---
+++ FormSwiftUI/CreateContactView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateContactView.swift
+//  FormSwiftUI
+//
+//  Created by Erik Loures on 03/06/23.
+//
+
+import SwiftUI
+
+struct CreateContactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateContactView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateContactView()
+    }
+}
---
+++ FormSwiftUI/Toggle+CheckBoxStyle.swift
@@ -0,0 +1,8 @@
+//
+//  Toggle+CheckBoxStyle.swift
+//  FormSwiftUI
+//
+//  Created by Erik Loures on 13/06/23.
+//
+
+import Foundation
