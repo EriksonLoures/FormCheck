cb0af1a4aa5be2643ef56b2427cf65cc1e5486bf
Erikson Loures <erikson992@gmail.com>
2023-06-13 23:36:27 -0300

    part1

--- FormSwiftUI/CreateContactView.swift
+++ FormSwiftUI/CreateContactView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct CreateContactView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var birthdate = Date()
+    @State private var address = ""
+    @State private var homeNumber = ""
+    @State private var whatsapp = ""
+    @State private var email = ""
+    @State private var isSelected: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("Dados Pessoais")) {
+                    TextField("Nome", text: $firstName)
+                    TextField("Sobrenome", text: $lastName)
+                    DatePicker("Data de nascimento", selection: $birthdate, displayedComponents: .date)
+                }
+                Section(header: Text("Endereco")) {
+                    TextField("Endereco", text: $address)
+                    TextField("Bairro", text: $homeNumber)
+                }
+                Section(header: Text("Contato")) {
+                    TextField("Whatsapp", text: $whatsapp)
+                    TextField("Email", text: $email)
+                    
+                }
+                
+                VStack {
+//                    Group {
+//                        Toggle("É cristão? ", isOn: $isSelected)
+//                        
+//
+//                    }
+                    Group {
+                        Toggle("aceita", isOn: $isSelected)                        
+                            .toggleStyle(.checklist)
+                            .font(.title)   
+                    }
+                }
+                
+            }
+            .navigationTitle("Cadastro")
+            
+            
+        }
     }
 }
-
 struct CreateContactView_Previews: PreviewProvider {
     static var previews: some View {
         CreateContactView()
     }
 }
+
+
--- FormSwiftUI/Toggle+CheckBoxStyle.swift
+++ FormSwiftUI/Toggle+CheckBoxStyle.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ToggleCheckBoxStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button {
+            configuration.isOn.toggle()
+        } label: {
+            Image(systemName: "checkmark.circle")
+                .symbolVariant(configuration.isOn ? .fill : .none)
+        }
+    }
+}
+
+extension ToggleStyle where Self == ToggleCheckBoxStyle {
+    static var checklist: ToggleCheckBoxStyle { .init()}
+}
